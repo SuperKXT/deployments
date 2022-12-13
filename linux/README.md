2276491a0434d0008702debaa934b4286cd5c54b
SuperKXT <superkxt@outlook.com>
2022-12-13 17:54:42 +0500

    Overhaul linux deployment script

---
+++ linux/README.md
@@ -0,0 +1,8 @@
+# Linux Deployments With Github Actions
+
+- Get the frontend and backend runner
+  - Go to `github.com/{user}/{repo}/settings/actions/runners/new`
+  - Copy the token from the end of first command in the Configure section ![image](../images/token.png)
+- Run the script
+- When prompted enter the frontend and token
+- Add the appropriate tags to the runner (`production` or `qa`)
--- linux/setup.sh
+++ linux/setup.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+echo "Frontend Details:"
+
+read -rp 'Github User For The Frontend [WiMetrix]:' frontend_user
+frontend_user=${frontend_user:-WiMetrix}
+
+while [ -z "${frontend_repo}" ]; do
+	read -rp 'Github Repo For The Frontend:' frontend_repo
+done
+
+while [ -z "${frontend_token}" ]; do
+	read -rp 'Access Token For The Frontend:' frontend_token
+done
+
+echo "Backend Details:"
+
+read -rp 'Github User For The Backend [WiMetrix]:' backend_user
+frontend_user=${backend_user:-WiMetrix}
+
+while [ -z "${backend_repo}" ]; do
+	read -rp 'Github Repo For The Backend:' backend_repo
+done
+
+while [ -z "${backend_token}" ]; do
+	read -rp 'Access Token For The Backend:' backend_token
+done
+
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
@@ -9,12 +35,6 @@ nvm install lts/*
 # enable corepack and pnpm
 corepack enable
 corepack prepare pnpm@latest --activate
-
-# create directories
-mkdir deployment
-cd deployment || exit
-mkdir frontend
-mkdir backend
 
 # install pm2 https://github.com/jessety/pm2-installer
 wget https://github.com/jessety/pm2-installer/archive/main.zip
@@ -27,3 +47,34 @@ npm run setup
 cd .. || exit
 rm -rf pm2-insaller-main
 # Copy and run any additional command you are asked to run after running the above
+
+# create directories
+mkdir frontend
+mkdir backend
+
+# Download the action runner
+curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
+# Optional: Validate the hash
+echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  runner.tar.gz" | shasum -a 256 -c
+
+# Setup frontend action runner
+cp -r ./runner.tar.gz ./frontend
+tar xzf ./frontend/runner.tar.gz
+rm -rf ./frontend/runner.tar.gz
+cd frontend || exit
+./config.sh --url https://github.com/"$frontend_user"/"$frontend_repo" --token "$frontend_token"
+sudo ./svc.sh install
+sudo ./svc.sh start
+sudo ./svc.sh status
+cd ..
+
+# Setup backend action runner
+cp -r ./runner.tar.gz ./backend
+tar xzf ./backend/runner.tar.gz
+rm -rf ./backend/runner.tar.gz
+cd backend || exit
+./config.sh --url https://github.com/"$backend_user"/"$backend_repo" --token "$backend_token"
+sudo ./svc.sh install
+sudo ./svc.sh start
+sudo ./svc.sh status
+cd ..
