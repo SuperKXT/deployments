807265749449d45539621054865d307fcde76e95
SuperKXT <superkxt@outlook.com>
2022-12-15 11:47:45 +0500

    Improve linux script and add vscode installation to it

--- linux/README.md
+++
@@ -1,8 +0,0 @@
-# Linux Deployments With Github Actions
-
-- Get the frontend and backend runner
-  - Go to `github.com/{user}/{repo}/settings/actions/runners/new`
-  - Copy the token from the end of first command in the Configure section ![image](../images/token.png)
-- Run the script
-- When prompted provide the frontend and backend config
-- Add the appropriate tags to the runner (`production` or `qa`)
--- setup.sh
+++ setup.sh
@@ -20,37 +20,50 @@ while [ -z "${backend_token}" ]; do
 	read -rp 'Access Token:' backend_token
 done
 
+echo 'Downloading NVM...'
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+echo 'NVM Installed!'
 
+echo 'Downloading Node...'
 # istall node
 nvm install lts/*
-
 # enable corepack and pnpm
 corepack enable
 corepack prepare pnpm@latest --activate
+echo 'Node Installed!'
 
+# Install VS Code
+echo 'Downloading VS Code...'
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -out code.deb
+./code.deb
+echo 'VS Code Installed!'
+
+echo 'Download PM2...'
 # install pm2 https://github.com/jessety/pm2-installer
 wget https://github.com/jessety/pm2-installer/archive/main.zip
 unzip main.zip
 rm -f main.zip
 cd pm2-installer-main || exit
 npm run configure
-npm run configure-policy
 npm run setup
 cd .. || exit
 rm -rf pm2-insaller-main
 # Copy and run any additional command you are asked to run after running the above
+echo 'PM2 Installed! And configured as a service'
 
 # create directories
 mkdir frontend
 mkdir backend
 
+echo 'Downloading Github Action Runner...'
 # Download the action runner
 curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
 # Optional: Validate the hash
 echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  runner.tar.gz" | shasum -a 256 -c
+echo 'Action Runner Downloaded!'
 
+echo 'Setting Up Frontend Runner...'
 # Setup frontend action runner
 cp -r ./runner.tar.gz ./frontend
 tar xzf ./frontend/runner.tar.gz
@@ -61,7 +74,9 @@ sudo ./svc.sh install
 sudo ./svc.sh start
 sudo ./svc.sh status
 cd ..
+echo 'Frontend Runner Started!'
 
+echo 'Setting Up Backend Runner...'
 # Setup backend action runner
 cp -r ./runner.tar.gz ./backend
 tar xzf ./backend/runner.tar.gz
@@ -72,3 +87,4 @@ sudo ./svc.sh install
 sudo ./svc.sh start
 sudo ./svc.sh status
 cd ..
+echo 'Backend Runner Started!'
