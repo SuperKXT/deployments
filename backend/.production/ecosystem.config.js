c96e0ecfb21d251be6e03dd0c8328050dd94e107
Saad Khan <superkxt@outlook.com>
2023-01-03 22:16:08 +0500

    Update backend ecosystem file

--- backend/.production/ecosystem.config.js
+++ backend/.production/ecosystem.config.js
@@ -3,8 +3,6 @@ const common = {
   instances: 3,
   Exec_mode: "cluster",
   env: {
-    NODE_ENV: "production",
-    PORT: 4005,
     IP: '0.0.0.0',
   },
 };
@@ -17,14 +15,7 @@ module.exports = {
       env: {
         ...common.env,
         NODE_ENV: 'production',
-      },
-    },
-    {
-      name: 'backend-dev',
-      ...common,
-      env: {
-        ...common.env,
-        NODE_ENV: 'development',
+        PORT: 4005,
       },
     },
     {
@@ -33,6 +24,16 @@ module.exports = {
       env: {
         ...common.env,
         NODE_ENV: 'qa',
+        PORT: 4006,
+      },
+    },
+    {
+      name: 'backend-dev',
+      ...common,
+      env: {
+        ...common.env,
+        NODE_ENV: 'development',
+        PORT: 4007,
       },
     },
   ]
