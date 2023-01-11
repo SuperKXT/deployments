a6c67e97af496218142818eecc3c78633e53ffd7
Saad Khan <superkxt@outlook.com>
2023-01-11 11:50:16 +0500

    Add jq installation to setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ while ! command -v curl &>/dev/null; do
 done
 echo -e "${GREEN}Curl Installed!${NC}"
 
+while ! command -v jq &>/dev/null; do
+	echo -e "\n${BLUE}Downloading Jq...${NC}"
+	sudo apt update
+	sudo apt install jq
+done
+echo -e "${GREEN}Jq Installed!${NC}"
+
 while ! command -v nvm &>/dev/null; do
 	echo -e "\n${BLUE}Downloading NVM...${NC}"
 	curl -o- --progress-bar https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
